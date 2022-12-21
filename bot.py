f3fc3d737a34756bd0e07ff4cc3dc9c2751e30ca
Rudy van Sloten <rdvansloten@outlook.com>
2022-12-22 00:09:43 +0100

    fix SR subcommand

--- bot.py
+++ bot.py
@@ -117,27 +117,27 @@ def get_splatfest():
    
   return message
 
-def get_schedule(category):
+def get_schedule(category, which=""):
   json_response = requests.get(f"https://splatoon3.ink/data/schedules.json").json()
   now = datetime.now(ZoneInfo("Europe/London")).strftime("%Y-%m-%dT%H:%M:%SZ")
-  utc_time = timezone(timedelta(hours=0), name="UTC")
+  # utc_time = timezone(timedelta(hours=0), name="UTC")
   
   if category == "salmon-run":
-  #   if args == "upcoming" or args == "":
-  #     message = f"**SALMON RUN (UPCOMING)**\n"
-  #     message += f"{get_schedule_time(category='range', start_time=salmon_run_schedule['startTime'], end_time=salmon_run_schedule['endTime'])}\n\n"
-  #     message += f"** {salmon_run_schedule['setting']['coopStage']['name']}: **\n"
-  #     for i in salmon_run_schedule["setting"]["weapons"]:
-  #       message += f"- [{i['name']}](https://splatoonwiki.org/wiki/{i['name'].replace(' ', '_')}) \n"
+    if which == "upcoming":
+      message = f"**SALMON RUN (UPCOMING)**\n"
+      message += f"{get_schedule_time(category='range', start_time=salmon_run_schedule['startTime'], end_time=salmon_run_schedule['endTime'])}\n\n"
+      message += f"** {salmon_run_schedule['setting']['coopStage']['name']}: **\n"
+      for i in salmon_run_schedule["setting"]["weapons"]:
+        message += f"- [{i['name']}](https://splatoonwiki.org/wiki/{i['name'].replace(' ', '_')}) \n"
 
-    # elif args == "current":
-    salmon_run_schedule = json_response["data"]["coopGroupingSchedule"]["regularSchedules"]["nodes"][0]
+    elif which == "":
+      salmon_run_schedule = json_response["data"]["coopGroupingSchedule"]["regularSchedules"]["nodes"][0]
 
-    message = f"**SALMON RUN**   _{get_schedule_time(category='ends', end_time=salmon_run_schedule['endTime'])}_\n"
-    message += f"{get_schedule_time(category='range', start_time=salmon_run_schedule['startTime'], end_time=salmon_run_schedule['endTime'])}\n\n"
-    message += f"** {salmon_run_schedule['setting']['coopStage']['name']}: **\n"
-    for i in salmon_run_schedule["setting"]["weapons"]:
-      message += f"- [{i['name']}](https://splatoonwiki.org/wiki/{i['name'].replace(' ', '_')}) \n"
+      message = f"**SALMON RUN**   _{get_schedule_time(category='ends', end_time=salmon_run_schedule['endTime'])}_\n"
+      message += f"{get_schedule_time(category='range', start_time=salmon_run_schedule['startTime'], end_time=salmon_run_schedule['endTime'])}\n\n"
+      message += f"** {salmon_run_schedule['setting']['coopStage']['name']}: **\n"
+      for i in salmon_run_schedule["setting"]["weapons"]:
+        message += f"- [{i['name']}](https://splatoonwiki.org/wiki/{i['name'].replace(' ', '_')}) \n"
 
     # for i in json_response["data"]["coopGroupingSchedule"]["regularSchedules"]["nodes"]:
     #   if now <= i["endTime"] and now >= i["startTime"]:
@@ -246,11 +246,8 @@ async def splatfest(interaction):
 
 @tree.command(name='salmon-run-beta')
 async def salmon_run_beta(interaction, subcommand: str):
-  if subcommand == 'upcoming':
-    # code to display upcoming Salmon Run events
-    pass
-  elif subcommand == 'current':
-    # code to display current Salmon Run event
+  if subcommand == "" or subcommand =="upcoming":
+    get_schedule(category="salmon-run", which=subcommand)
     pass
   else:
     await interaction.response.send_message('Invalid subcommand. Please use "upcoming" or "current".')
