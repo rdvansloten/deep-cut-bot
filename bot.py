6bd6219a3399d15d97b074a3957fefeb952ef348
Rudy van Sloten <rdvansloten@outlook.com>
2022-12-22 00:20:36 +0100

    Fix command order

--- bot.py
+++ bot.py
@@ -245,7 +245,7 @@ async def splatfest(interaction):
     await interaction.response.send_message(get_splatfest(), suppress_embeds=True)
 
 @tree.command(name='salmon-run-beta', description="Get current or upcoming Salmon Run schedule.")
-async def salmon_run_beta(interaction, subcommand: str):
+async def salmon_run_beta(interaction, subcommand=""):
   if subcommand == "" or subcommand =="upcoming":
     get_schedule(category="salmon-run", which=subcommand)
     pass
