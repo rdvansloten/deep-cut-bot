8b28928eb548d33c78907fbaed277b0972973887
Rudy van Sloten <rdvansloten@outlook.com>
2023-01-28 02:20:19 +0800

    fix url

--- bot.py
+++ bot.py
@@ -389,7 +389,7 @@ async def salmon_run_schedule():
       embeds = []
         
       for i in salmon_run_schedule["setting"]["weapons"]:        
-        embed = discord.Embed(description=get_html.get_weapon_description(i['name'].replace(' ', '_')))
+        embed = discord.Embed(description=get_html.get_weapon_description(f"https://splatoonwiki.org/wiki/{i['name'].replace(' ', '_')}"))
         # Set the thumbnail
         embed.set_thumbnail(url=f"{i['image']['url']}")
         # Set the title and URL
